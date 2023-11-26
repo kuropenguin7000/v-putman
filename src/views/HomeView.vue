92a44fa99a67848b5cc10f1cbf9d27e2ab637d81
rafi.rahman <rafi.rahman@gdn-commerce.com>
2023-11-26 16:26:44 +0700

    add prettier, tailwind, and remove unnecessary files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="text-9xl text-amber-800">hello world</div>
   </main>
 </template>
