e3faf087af7556eb5b3fbcedc77ea98eb198fa79
rafi.rahman <rafi.rahman@gdn-commerce.com>
2023-12-20 21:46:57 +0700

    dark: http status method name colors

--- src/components/request/TheRequest.vue
+++ src/components/request/TheRequest.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import type { Ref } from 'vue'
 import { httpStatusList } from '@/constants/http-status'
 import ChevronDown from '@/components/icons/ChevronDown.vue'
 
+interface HttpStatus {
+  name: string
+  color: string
+}
+
 const url: Ref<string> = ref('')
-const selectedHttpStatus: Ref<string> = ref('GET')
+const selectedHttpStatus: HttpStatus = reactive({
+  name: 'GET',
+  color: 'text-[#6BDD9A]'
+})
 const isDropdownVisible: Ref<boolean> = ref(false)
 const httpStatusDropdown = ref<HTMLInputElement | null>(null)
 
 function onClickHttpMethod(): void {
   isDropdownVisible.value = !isDropdownVisible.value
 }
-function handleDropdownClick(event: MouseEvent): void {
-  selectedHttpStatus.value = (event.target as HTMLInputElement).textContent!
+function handleDropdownClick(httpStatus): void {
+  selectedHttpStatus.name = httpStatus.name
+  selectedHttpStatus.color = httpStatus.color
 }
 function clickOutside() {
   isDropdownVisible.value = false
@@ -31,19 +40,23 @@ function clickOutside() {
         >
         <button
           v-click-outside="clickOutside"
-          class="dark:bg-dark-primary z-10 inline-flex w-1/12 flex-shrink-0 items-center justify-between rounded-s-lg border border-e-0 border-gray-300 bg-gray-100 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-700"
+          class="z-10 inline-flex w-1/12 flex-shrink-0 items-center justify-between rounded-s-lg border border-e-0 border-gray-300 bg-gray-100 px-4 py-2.5 text-center text-sm font-medium hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:bg-dark-primary dark:hover:bg-gray-700 dark:focus:ring-blue-700"
           type="button"
           @click="onClickHttpMethod"
         >
-          {{ selectedHttpStatus }}
-          <ChevronDown />
+          <label :class="selectedHttpStatus.color">
+            {{ selectedHttpStatus.name }}
+          </label>
+          <div class="text-[#A6A6A6]">
+            <ChevronDown />
+          </div>
         </button>
         <div class="relative w-full">
           <input
             v-model="url"
             type="search"
             id="search-dropdown"
-            class="rounded-s-gray-100 rounded-s-2 dark:bg-dark-primary z-20 block w-full rounded-e-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
+            class="rounded-s-gray-100 rounded-s-2 z-20 block w-full rounded-e-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-dark-primary dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
             placeholder="Enter URL or paste text"
             required
           />
@@ -61,15 +74,15 @@ function clickOutside() {
       ref="httpStatusDropdown"
       class="z-10 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
     >
-      <ul
-        class="py-2 text-sm text-gray-700 dark:text-gray-200"
-        @click="handleDropdownClick"
-      >
-        <li v-for="httpStatus in httpStatusList" :key="httpStatus">
+      <ul class="py-2 text-sm text-gray-700 dark:text-gray-200">
+        <li v-for="httpStatus in httpStatusList" :key="httpStatus.name">
           <a
-            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >{{ httpStatus }}</a
+            class="block px-4 py-2 hover:cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600"
+            :class="httpStatus.color"
+            @click="handleDropdownClick(httpStatus)"
           >
+            {{ httpStatus.name }}
+          </a>
         </li>
       </ul>
     </div>
--- src/constants/http-status.ts
+++ src/constants/http-status.ts
@@ -1,9 +1,9 @@
-export const httpStatusList: string[] = [
-  'GET',
-  'POST',
-  'PUT',
-  'PATCH',
-  'DELETE',
-  'HEAD',
-  'OPTIONS'
+export const httpStatusList = [
+  { name: 'GET', color: 'text-[#6BDD9A]' },
+  { name: 'POST', color: 'text-[#FFE47E]' },
+  { name: 'PUT', color: 'text-[#74AEF6]' },
+  { name: 'PATCH', color: 'text-[#C0A8E1]' },
+  { name: 'DELETE', color: 'text-[#F79A8E]' },
+  { name: 'HEAD', color: 'text-[#6BDD9A]' },
+  { name: 'OPTIONS', color: 'text-[#F15EB0]' }
 ]
