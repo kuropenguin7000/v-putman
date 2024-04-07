045d4c0bbfc01e7d0f6d00fcb5f658d8e5032adc
rafi.rahman <ctlveccc@gmail.com>
2024-04-07 11:16:56 +0700

    adjust DarkModeToggle.spec.ts and enabled unit test pre-commit

--- src/components/__tests__/common/DarkModeToggle.spec.ts
+++ src/components/__tests__/common/DarkModeToggle.spec.ts
@@ -1,13 +1,10 @@
 import { expect } from 'vitest'
 import { mount, VueWrapper } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
-import { useDarkStore } from '@/stores/dark'
 import DarkModeToggle from '@/components/common/DarkModeToggle.vue'
 
 describe('DarkModeToggle', () => {
   let wrapper: VueWrapper
-  let instance: any
-  let store: any
 
   beforeEach((): void => {
     wrapper = mount(DarkModeToggle, {
@@ -15,20 +12,17 @@ describe('DarkModeToggle', () => {
         plugins: [createTestingPinia()]
       }
     })
-    instance = wrapper.vm as any
-    store = useDarkStore()
   })
 
-  it('should test isDarkMode value is false', async () => {
-    expect(store.getDarkMode).to.be.false
-    expect(instance.isDarkMode).to.be.false
-    expect(wrapper.find('label').attributes('class')).to.contain('bg-day')
+  it('renders correctly', () => {
+    wrapper = mount(DarkModeToggle)
+    expect(wrapper.exists()).to.be.true
   })
 
-  it('should test isDarkMode value is true', async () => {
-    await wrapper.find('input').trigger('click')
-    expect(store.getDarkMode).to.be.true
-    expect(instance.isDarkMode).to.be.true
-    expect(wrapper.find('label').attributes('class')).to.contain('bg-night')
+  it('toggles dark mode when clicked', async () => {
+    wrapper = mount(DarkModeToggle)
+    expect(document.documentElement.classList.contains('dark')).to.be.true
+    await wrapper.find('input[type="checkbox"]').trigger('click')
+    expect(document.documentElement.classList.contains('dark')).to.be.false
   })
 })
